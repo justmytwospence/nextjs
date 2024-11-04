c8ab3974a77dca689beb217b0a110918d88ad5d3
Spencer Boucher <github@spencerboucher.com>
2024-11-04 10:05:34 -0700

    Revert spinner changes

--- components/ui/spinner.jsx
+++ components/ui/spinner.jsx
@@ -1,7 +1,19 @@
-import { Loader } from "shadcn/ui";
-
 export function Spinner({ className, ...props }) {
   return (
-    <Loader className={`animate-spin ${className}`} {...props} />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={`animate-spin ${className}`}
+      {...props}
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+    </svg>
   );
 }
