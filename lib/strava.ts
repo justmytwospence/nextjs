510e8155070b26999ad66eeac9eebc52beb65b54
Spencer Boucher <github@spencerboucher.com>
2024-11-04 10:25:21 -0700

    Set per_page to 10 for developing on this branch

--- lib/strava.ts
+++ lib/strava.ts
@@ -7,7 +7,7 @@ import tj from '@mapbox/togeojson';
 import { DOMParser } from '@xmldom/xmldom';
 import { Session } from 'next-auth';
 
-export async function fetchUserRoutes(session: Session, per_page: number = 200): Promise<AthleteRoute[]> {
+export async function fetchUserRoutes(session: Session, per_page: number = 10): Promise<AthleteRoute[]> {
   const sessionLogger = createSessionLogger(session);
   sessionLogger.info('Fetching user routes from Strava', { per_page });
   try {
