b70e3f17d8a42c7560ca17c97298a320cf61b10b
Spencer Boucher <github@spencerboucher.com>
2024-11-04 17:22:57 -0700

    Update Strava webhook event schemas to use boolean and bigint types

--- schemas/strava-webhook-events.ts
+++ schemas/strava-webhook-events.ts
@@ -3,16 +3,16 @@ import * as z from "zod";
 export const WebhookEventUpdatesSchema = z.object({
   title: z.string().optional(),
   type: z.string().optional(),
-  private: z.string().optional(),
+  private: z.boolean().optional(),
   authorized: z.literal('false').optional(),
 });
 
 export const WebhookEventSchema = z.object({
   aspect_type: z.enum(['create', 'update', 'delete']),
-  event_time: z.number(),
-  object_id: z.number(),
+  event_time: z.bigint(),
+  object_id: z.bigint(),
   object_type: z.enum(['activity', 'athlete']),
-  owner_id: z.number(),
+  owner_id: z.bigint(),
   subscription_id: z.number(),
   updates: WebhookEventUpdatesSchema.optional(),
 });
