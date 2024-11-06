cd7c0ab12988fa77618ee1624cfad8776dd6df93
Spencer Boucher <github@spencerboucher.com>
2024-11-06 08:56:23 -0700

    Update ignore-build script to allow builds on main and staging branches

--- scripts/ignore-build.sh
+++ scripts/ignore-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$VERCEL_GIT_COMMIT_REF" != "main" ] && [ "$VERCEL_GIT_COMMIT_REF" != "staging" ]; then
+if [ "$VERCEL_GIT_COMMIT_REF" == "main" ] || [ "$VERCEL_GIT_COMMIT_REF" == "staging" ]; then
   exit 1
 else
   exit 0
