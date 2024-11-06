2ca257f131d0d80ee0cd5c62e6642e86a0234979
Spencer Boucher <github@spencerboucher.com>
2024-11-06 11:10:53 -0700

    Add error handling to account deletion

--- lib/strava-webhook.ts
+++ lib/strava-webhook.ts
@@ -21,18 +21,22 @@ export default async function processWebhookEvent(event: WebhookEvent) {
       baseLogger.info(`Received athlete event: ${JSON.stringify(event, null, 2)}`);
       if (event.updates?.authorized === "false") {
         baseLogger.info(`Deauthorizing user with stravaId: ${event.object_id}`);
-        await prisma.account.delete({
-          where: {
-            provider_providerAccountId: {
-              provider: "strava",
-              providerAccountId: String(event.object_id),
+        try {
+          await prisma.account.delete({
+            where: {
+              provider_providerAccountId: {
+                provider: "strava",
+                providerAccountId: String(event.object_id),
+              },
             },
-          },
-          include: {
-            user: true,
-          },
-        });
-        baseLogger.info(`Deleted user and associated data for stravaId: ${event.object_id}`);
+            include: {
+              user: true,
+            },
+          });
+          baseLogger.info(`Deleted user and associated data for stravaId: ${event.object_id}`);
+        } catch (error) {
+          baseLogger.error(`Failed to deauthorize user with stravaId: ${event.object_id}: ${error}`);
+        }
       }
       break;
   }
