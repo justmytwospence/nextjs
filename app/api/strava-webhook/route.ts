fd5f7101a2bfd29c617856bd81b5f914bc978451
Spencer Boucher <github@spencerboucher.com>
2024-11-06 10:01:56 -0700

    Remove origin check from GET

--- app/api/strava-webhook/route.ts
+++ app/api/strava-webhook/route.ts
@@ -32,9 +32,9 @@ export async function GET(request: NextRequest) {
   const hub_verify_token = searchParams.get("hub.verify_token");
   const hub_challenge = searchParams.get("hub.challenge");
 
-  if (!isStravaOrigin(request) && hub_verify_token == process.env.STRAVA_WEBHOOK_VERIFY_TOKEN) {
-    return new NextResponse(null, { status: 403 });
-  }
+  // if (!isStravaOrigin(request) && hub_verify_token == process.env.STRAVA_WEBHOOK_VERIFY_TOKEN) {
+  //   return new NextResponse(null, { status: 403 });
+  // }
 
   if (hub_mode === "subscribe" && hub_verify_token === process.env.STRAVA_WEBHOOK_VERIFY_TOKEN) {
     return NextResponse.json({ "hub.challenge": hub_challenge });
