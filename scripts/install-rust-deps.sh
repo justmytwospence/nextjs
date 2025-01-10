d18e491e2fe7bfaa2064f79b6a88c54844f875d5
Spencer Boucher <github@spencerboucher.com>
2025-01-10 13:42:40 -0700

    Update install-rust-deps.sh to include pkg-config in conda environment creation

--- scripts/install-rust-deps.sh
+++ scripts/install-rust-deps.sh
@@ -2,7 +2,7 @@
 
 wget --quiet -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 
 bash miniconda.sh -b > /dev/null
-$HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge clang libcxx libcxxabi gdal -y 
+$HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge pkg-config clang libcxx libcxxabi gdal -y 
 export GDAL_HOME="$HOME/miniconda3/envs/gdal_env"
 echo $(ls $GDAL_HOME)
 echo $(ls $GDAL_HOME/include)
