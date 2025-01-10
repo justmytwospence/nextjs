f15be31a301f8d239e80b78c4820ce7871beed82
Spencer Boucher <github@spencerboucher.com>
2025-01-10 13:20:23 -0700

    Update install-gdal.sh to include glibc in conda environment creation

--- scripts/install-gdal.sh
+++ scripts/install-gdal.sh
@@ -2,6 +2,6 @@
 
 wget --quiet -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 
 bash miniconda.sh -b > /dev/null
-$HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge gdal -y 
+$HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge gdal glibc -y 
 export GDAL_HOME="$HOME/miniconda3/envs/gdal_env"
 echo $(ls $GDAL_HOME)
