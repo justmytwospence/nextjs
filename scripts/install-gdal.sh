067f17edce55b3dc5dbb26b102ed2ae7f7cf3b08
Spencer Boucher <github@spencerboucher.com>
2025-01-10 13:02:00 -0700

    Add GDAL_LIB_DIR export and list contents of GDAL_INCLUDE_DIR and GDAL_LIB_DIR in install-gdal.sh

--- scripts/install-gdal.sh
+++ scripts/install-gdal.sh
@@ -4,3 +4,6 @@ wget --quiet -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-late
 bash miniconda.sh -b > /dev/null
 $HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge gdal -y 
 export GDAL_INCLUDE_DIR="$HOME/miniconda3/envs/gdal_env/include"
+export GDAL_LIB_DIR="$HOME/miniconda3/envs/gdal_env/lib"
+echo $(ls $GDAL_INCLUDE_DIR)
+echo $(ls $GDAL_LIB_DIR)
