ffd28a6dbf81886f791ce80d87c77463b584d19a
Spencer Boucher <github@spencerboucher.com>
2025-01-10 12:13:07 -0700

    Add shebang to install-gdal.sh and improve GDAL installation script with quiet mode

--- scripts/install-gdal.sh
+++ scripts/install-gdal.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget --quiet -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 
 bash miniconda.sh -b > /dev/null
 $HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge gdal -y 
