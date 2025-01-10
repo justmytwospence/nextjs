2dcacb8c6ea54935b1fbea15fc5a66fc1be70728
Spencer Boucher <github@spencerboucher.com>
2025-01-10 12:58:25 -0700

    Update build script to run npm build in gdal_env and set GDAL_INCLUDE_DIR in install-gdal.sh

--- scripts/install-gdal.sh
+++ scripts/install-gdal.sh
@@ -3,3 +3,4 @@
 wget --quiet -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 
 bash miniconda.sh -b > /dev/null
 $HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge gdal -y 
+export GDAL_INCLUDE_DIR="$HOME/miniconda3/envs/gdal_env/include"
