bd4de2289cbc5e8352df7285951a7db5186e96ce
Spencer Boucher <github@spencerboucher.com>
2025-01-10 13:09:45 -0700

    Update build script to use GDAL_HOME instead of GDAL_INCLUDE_DIR in build:rs command

--- scripts/install-gdal.sh
+++ scripts/install-gdal.sh
@@ -3,7 +3,5 @@
 wget --quiet -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 
 bash miniconda.sh -b > /dev/null
 $HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge gdal -y 
-export GDAL_INCLUDE_DIR="$HOME/miniconda3/envs/gdal_env/include"
-export GDAL_LIB_DIR="$HOME/miniconda3/envs/gdal_env/lib"
-echo $(ls $GDAL_INCLUDE_DIR)
-echo $(ls $GDAL_LIB_DIR)
+export GDAL_HOME="$HOME/miniconda3/envs/gdal_env"
+echo $(ls $GDAL_HOME)
