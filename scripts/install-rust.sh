a0c7e966af55521c34ea8e51796ef4c393364b5a
Spencer Boucher <github@spencerboucher.com>
2025-01-09 20:44:34 -0700

    Refactor Rust installation script to streamline Miniconda and clang installation

--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -sSf | sh  -s 
+conda install -c conda-forge clang
+
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
-
-curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh | bash
-conda install -c conda-forge clang
