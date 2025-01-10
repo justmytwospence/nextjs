6f848bf92806ce8de2a696066f9ac3e3ac920b49
Spencer Boucher <github@spencerboucher.com>
2025-01-09 20:49:39 -0700

    Update build script to set LIBCLANG_PATH and add debug output in Rust installation script

--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -3,5 +3,8 @@
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -sSf | sh  -s 
 conda install -c conda-forge clang
 
+echo "conda info --base"
+echo `conda info ---base`
+
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
