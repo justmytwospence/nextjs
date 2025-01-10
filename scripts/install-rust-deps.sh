a0b6d6c2cbf1fe26992480ef8f10afd88341c819
Spencer Boucher <github@spencerboucher.com>
2025-01-10 14:35:53 -0700

    Refactor build scripts: streamline build command and update Rust installation process

--- scripts/install-rust-deps.sh
+++ scripts/install-rust-deps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+rustup toolchain install stable
 curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash miniconda.sh -b > /dev/null
 $HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge pkg-config clang libcxx libcxxabi gdal -y 
-export GDAL_HOME="$HOME/miniconda3/envs/gdal_env"
 echo $(ls $GDAL_HOME)
 echo $(ls $GDAL_HOME/include)
--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-export PATH="$HOME/.cargo/bin:$PATH"
