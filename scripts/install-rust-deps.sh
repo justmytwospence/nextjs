adf63b281af1397afe3391b8f1b836e3ca0cd9e3
Spencer Boucher <github@spencerboucher.com>
2025-01-10 16:04:04 -0700

    Update build scripts: modify build:rs command and enhance install-rust-deps.sh for artifact management

--- scripts/install-rust-deps.sh
+++ scripts/install-rust-deps.sh
@@ -4,5 +4,6 @@ rustup toolchain install stable
 curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash miniconda.sh -b > /dev/null
 $HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge pkg-config clang libcxx libcxxabi gdal -y 
-echo $(ls $GDAL_HOME)
-echo $(ls $GDAL_HOME/include)
+ls $HOME/miniconda3/envs/gdal_env/lib
+mkdir -p artifacts/ 
+cp -r $HOME/miniconda3/envs/gdal_env/lib/libgdal.so artifacts/
