37f4f642a773f12bb651585e0e426c8365db1fa5
Spencer Boucher <github@spencerboucher.com>
2025-01-10 14:12:47 -0700

    Remove unnecessary commands from install-rust.sh and ensure PATH is set correctly

--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -2,6 +2,3 @@
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
-mkdir -p /tmp/apt/lists/partial
-APT_CONFIG=/tmp/apt/apt.conf apt-get update
-apt-get install -y libclang-dev
