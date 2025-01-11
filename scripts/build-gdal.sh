aff5260a5c412783f4fb4511f259870c4e352e0d
Spencer Boucher <github@spencerboucher.com>
2025-01-11 12:14:16 -0700

    Update build-gdal script to change project dependencies and adjust CMake options for static build

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -1,4 +1,4 @@
-# proj
+# dependencies
 curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash miniconda.sh -b > /dev/null
 $HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge libjxl pkg-config libtiff proj -y
@@ -13,12 +13,10 @@ $HOME/miniconda3/bin/conda run -n gdal_env cmake .. \
   -BUILD_SHARED_LIBS=OFF \
   -DENABLE_STATIC_LIB=ON \
   -BUILD_PYTHON_BINDINGS:BOOL=OFF \
-  -GDAL_USE_EXTERNAL_LIBS:BOOL=ON \
   -DGDAL_BUILD_OPTIONAL_DRIVERS=OFF \
   -DOGR_BUILD_OPTIONAL_DRIVERS=OFF \
   -DOGR_ENABLE_DRIVER_MEM=ON \
   -DOGR_ENABLE_DRIVER_GTIFF=ON \
-  -GDAL_USE_TIFF_INTERNAL=ON \
   -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=. 
 cmake --build . --target GDAL --config Release
 ls -lah .
