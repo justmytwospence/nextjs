c3202769f6d93c19f461e67e31b16a0e17e8e85f
Spencer Boucher <github@spencerboucher.com>
2025-01-11 16:43:37 -0700

    Add libwebp build steps and enable webp support in GDAL build script

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -12,20 +12,33 @@ $HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge \
   pkg-config \
   proj -y
 
-# Build libdeflate
+# libdeflate
 curl -LO https://github.com/ebiggers/libdeflate/archive/v1.19.tar.gz
 tar xf v1.19.tar.gz
 cd libdeflate-1.19
 mkdir build && cd build
+echo "Building libdeflate"
 cmake .. \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
   -DLIBDEFLATE_BUILD_STATIC_LIB=ON \
   -DLIBDEFLATE_BUILD_SHARED_LIB=OFF
 make -j4
-echo "Building libdeflate"
 mv libdeflate.a ../..
 cd ../..
 rm -rf libdeflate-1.19
+
+# webp
+curl -LO https://github.com/webmproject/libwebp/archive/refs/tags/v1.5.0.tar.gz
+tar xf libwebp-1.5.0.tar.gz
+cd libwebp-1.5.0
+mkdir build && cd build
+echo "Building libwebp"
+cmake .. \
+  -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+make -j4
+mv src/libwebp.a ../..
+cd ../..
+rm -rf libwebp-1.5.0
 
 # gdal
 curl -LO https://github.com/OSGeo/gdal/releases/download/v3.10.0/gdal-3.10.0.tar.gz
@@ -40,11 +53,13 @@ $HOME/miniconda3/bin/conda run -n gdal_env cmake .. \
   -DCMAKE_CXX_FLAGS="-fPIC" \
   -DCMAKE_C_FLAGS="-fPIC" \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-  -DDeflate_LIBRARY_RELEASE=../../libdeflate.a \
   -DGDAL_BUILD_OPTIONAL_DRIVERS=OFF \
   -DGDAL_ENABLE_DRIVER_GTIFF=ON \
   -DGDAL_ENABLE_DRIVER_MEM=ON \
   -DGDAL_USE_DEFLATE=ON \
+  -DDeflate_LIBRARY_RELEASE=../../libdeflate.a \
+  -DGDAL_USE_WEBP=ON \
+  -DWEBP_LIBRARY=../../libwebp.a \
   -DGDAL_USE_INTERNAL_LIBS=ON \
   -DOGR_BUILD_OPTIONAL_DRIVERS=OFF
 echo "Building GDAL"
