7eaeeebdb7e3491f12dcfb0381f30d9e43a6fd71
Spencer Boucher <github@spencerboucher.com>
2025-01-11 12:28:37 -0700

    Update build-gdal script to enable verbose output during GDAL build process

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -19,5 +19,5 @@ $HOME/miniconda3/bin/conda run -n gdal_env cmake .. \
   -DGDAL_ENABLE_DRIVER_MEM=ON \
   -DOGR_BUILD_OPTIONAL_DRIVERS=OFF
 echo "Building GDAL"
-cmake --build . --target GDAL
+cmake --build . --target GDAL -- VERBOSE=1
 ls -lah .
