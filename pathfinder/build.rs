6ca71bbb0e5f641706e362c50a597dd7edbd88be
Spencer Boucher <github@spencerboucher.com>
2025-01-10 23:07:56 -0700

    Add RUSTFLAGS and update build scripts to link with artifacts directory

--- pathfinder/build.rs
+++ pathfinder/build.rs
@@ -1,5 +1,8 @@
 extern crate napi_build;
 
 fn main() {
+  println!("cargo:rustc-link-search=native=/var/task/artifacts"); // Directory with shared libraries
+  println!("cargo:rustc-link-lib=dylib=gdal"); // Link dynamically with libgdal.so
+  println!("cargo:rustc-link-arg=-Wl,-rpath,/var/task/artifacts"); // Set RPATH
   napi_build::setup();
 }
