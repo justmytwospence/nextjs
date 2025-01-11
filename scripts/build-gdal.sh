e5b644c7cf581270ae29a1b450d5e863c102d2b0
Spencer Boucher <github@spencerboucher.com>
2025-01-11 13:32:41 -0700

    Cleanup build-gdal script by removing unnecessary echo and ls commands

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -21,7 +21,3 @@ $HOME/miniconda3/bin/conda run -n gdal_env cmake .. \
   -DOGR_BUILD_OPTIONAL_DRIVERS=OFF
 echo "Building GDAL"
 cmake --build . --target GDAL
-echo $PWD
-ls -lah $PWD
-echo $PWD/build
-ls -lah $PWD/build
