5339ebb03467d07a20ac3b4d5844beaffee038aa
Spencer Boucher <github@spencerboucher.com>
2025-01-11 11:20:45 -0700

    Update build-gdal script to include libjxl in GDAL environment setup

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -1,7 +1,7 @@
 # proj
 curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash miniconda.sh -b > /dev/null
-$HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge pkg-config libtiff proj -y
+$HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge libjxl pkg-config libtiff proj -y
 
 curl -Lo gdal-3.10.0.tar.gz https://github.com/OSGeo/gdal/releases/download/v3.10.0/gdal-3.10.0.tar.gz
 tar -xf gdal-3.10.0.tar.gz
