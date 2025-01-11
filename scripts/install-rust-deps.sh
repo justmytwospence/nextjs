747169660eac3b57062767d89bdeadf499b79a34
Spencer Boucher <github@spencerboucher.com>
2025-01-10 23:37:16 -0700

    Update install script to copy GDAL library dependencies to artifacts directory

--- scripts/install-rust-deps.sh
+++ scripts/install-rust-deps.sh
@@ -6,9 +6,15 @@ bash miniconda.sh -b > /dev/null
 $HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge pkg-config clang libcxx libcxxabi gdal -y 
 ls $HOME/miniconda3/envs/gdal_env/lib
 ldd $HOME/miniconda3/envs/gdal_env/lib/libgdal.so.36
+
 mkdir -p artifacts/
-cp -r $HOME/miniconda3/envs/gdal_env/lib/* artifacts/
+ldd $HOME/miniconda3/envs/gdal_env/lib/libgdal.so.36 | grep "=> /" | awk '{print $3}' | while read -r lib; do
+    echo "Copying dependency: $lib"
+    cp "$lib" artifacts/
+done
 chmod +x artifacts/*
+
 ls -lah artifacts
 ldd artifacts/libgdal.so.36
+
 echo $RUSTFLAGS
