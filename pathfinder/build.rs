334f3f5bbe7c81a7cec6ed7e15328b12267fcfa8
Spencer Boucher <github@spencerboucher.com>
2025-01-10 23:12:47 -0700

    Update RUSTFLAGS to use relative path for artifacts directory

--- pathfinder/build.rs
+++ pathfinder/build.rs
@@ -3,6 +3,6 @@ extern crate napi_build;
 fn main() {
   println!("cargo:rustc-link-search=native=/var/task/artifacts"); // Directory with shared libraries
   println!("cargo:rustc-link-lib=dylib=gdal"); // Link dynamically with libgdal.so
-  println!("cargo:rustc-link-arg=-Wl,-rpath,/var/task/artifacts"); // Set RPATH
+  println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/../artifacts"); // Set RPATH
   napi_build::setup();
 }
