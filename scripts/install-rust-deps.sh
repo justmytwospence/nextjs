5303e6a01a5504b957d993b335515ce9580046aa
Spencer Boucher <github@spencerboucher.com>
2025-01-10 23:26:43 -0700

    Add ldd command to check GDAL library dependencies in install script

--- scripts/install-rust-deps.sh
+++ scripts/install-rust-deps.sh
@@ -5,6 +5,7 @@ curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Li
 bash miniconda.sh -b > /dev/null
 $HOME/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge pkg-config clang libcxx libcxxabi gdal -y 
 ls $HOME/miniconda3/envs/gdal_env/lib
+ldd $HOME/miniconda3/envs/gdal_env/lib/libgdal.so.36
 mkdir -p artifacts/
 cp -r $HOME/miniconda3/envs/gdal_env/lib/* artifacts/
 chmod +x artifacts/*
