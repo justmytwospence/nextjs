540c2ba211655303f8802f8aae8d96f1d660827f
Spencer Boucher <github@spencerboucher.com>
2025-01-11 11:58:34 -0700

    Fix build-gdal script to correct CMake options for static library configuration

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -9,10 +9,10 @@ cd gdal-3.10.0
 mkdir build
 cd build
 $HOME/miniconda3/bin/conda run -n gdal_env cmake .. \
-  -DBUILD_APPS=OFF \                
-  -DBUILD_SHARED_LIBS=OFF \         
+  -BUILD_APPS=OFF \                
+  -BUILD_SHARED_LIBS=OFF \         
   -DENABLE_STATIC_LIB=ON \           
-  -BUILD_PYTHON_BINDINGS=OFF \
+  -BUILD_PYTHON_BINDINGS:BOOL=OFF
   -DGDAL_BUILD_OPTIONAL_DRIVERS=OFF \
   -DOGR_BUILD_OPTIONAL_DRIVERS=OFF \
   -DOGR_ENABLE_DRIVER_MEM=ON \
