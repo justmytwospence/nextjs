2090299d0fda50a13771ed82b48f32a6b3ccb7fb
Spencer Boucher <github@spencerboucher.com>
2025-01-11 21:20:40 -0700

    Remove static linking for stdc++ in build script

--- pathfinder/build.rs
+++ pathfinder/build.rs
@@ -3,7 +3,6 @@ extern crate napi_build;
 fn main() { 
   println!("cargo:rustc-link-search=native=/opt/build/repo");
   println!("cargo:rustc-link-lib=static=gdal");
-  println!("cargo:rustc-link-lib=static=stdc++"); 
   println!("cargo:rustc-link-arg=-Wl,-rpath,/var/task/dylibs");
   napi_build::setup();
 }
