45ce46f587e6964308a6d0d3128fb09d00e5768e
Spencer Boucher <github@spencerboucher.com>
2025-01-12 08:53:34 -0700

    Update copy-dylibs.sh to use conda environment for ldd and streamline GDAL dependency copying

--- scripts/copy-dylibs.sh
+++ scripts/copy-dylibs.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-set -e  # Exit on error
+set -e  
 
-ldd pathfinder/pathfinder.linux-x64-gnu.node
-nm -C pathfinder/pathfinder.linux-x64-gnu.node | grep GDAL
+$"HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node ldd pathfinder/pathfinder.linux-x64-gnu.node 
 
 mkdir dylibs
 
-echo "Copying .node dynamic dependencies"
-ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
+# copy GDAL dependencies
+echo "Copying GDAL dependencies"
+$"HOME"/miniconda3/bin/conda run -n gdal_env ldd gdal-3.10.0/build/lib/libgdal.so | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
   echo "Copying $lib"
-  cp "$lib" /opt/build/repo/dylibs/
+  cp "$lib" dylibs/
+done
+
+# copy .node dependencies
+echo "Copying .node dynamic dependencies"
+$"HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
+  echo "Copying $lib"
+  cp "$lib" dylibs/
 done
