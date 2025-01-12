2d517aa59174a8792b6391ab69b0f6b841d29222
Spencer Boucher <github@spencerboucher.com>
2025-01-12 11:02:14 -0700

    Add libcurl to dependencies and enable GDAL_USE_CURL option in build-gdal.sh

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -11,7 +11,9 @@ curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Li
 bash miniconda.sh -b > /dev/null
 "$HOME"/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge \
   clang \
+  gdal \
   geos \
+  libcurl \
   libcxx \
   libcxxabi \
   libdeflate \
@@ -110,6 +112,7 @@ mkdir build && cd build
   -DGDAL_BUILD_OPTIONAL_DRIVERS=OFF \
   -DGDAL_ENABLE_DRIVER_GTIFF=ON \
   -DGDAL_ENABLE_DRIVER_MEM=ON \
+  -DGDAL_USE_CURL=ON \
   -DGDAL_USE_DEFLATE=ON \
   -DGDAL_USE_GEOS=ON \
   -DGDAL_USE_INTERNAL_LIBS=ON \
