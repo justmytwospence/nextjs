8d6253477c02582a49ebf03f52270a6ac49bbbde
Spencer Boucher <github@spencerboucher.com>
2025-01-12 09:00:02 -0700

    Fix quoting issue in copy-dylibs.sh to ensure proper execution of ldd commands

--- scripts/copy-dylibs.sh
+++ scripts/copy-dylibs.sh
@@ -2,20 +2,20 @@
 
 set -e  
 
-$"HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node ldd pathfinder/pathfinder.linux-x64-gnu.node 
+"$HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node ldd pathfinder/pathfinder.linux-x64-gnu.node 
 
 mkdir dylibs
 
 # copy GDAL dependencies
 echo "Copying GDAL dependencies"
-$"HOME"/miniconda3/bin/conda run -n gdal_env ldd gdal-3.10.0/build/lib/libgdal.so | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
+"$HOME"/miniconda3/bin/conda run -n gdal_env ldd gdal-3.10.0/build/lib/libgdal.so | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
   echo "Copying $lib"
   cp "$lib" dylibs/
 done
 
 # copy .node dependencies
 echo "Copying .node dynamic dependencies"
-$"HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
+"$HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
   echo "Copying $lib"
   cp "$lib" dylibs/
 done
