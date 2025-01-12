0ac3bb8ab0a4445114cbad97208aa4da16cdd623
Spencer Boucher <github@spencerboucher.com>
2025-01-12 13:22:55 -0700

    Update copy-dylibs.sh to filter GDAL dependencies by miniconda3 path

--- scripts/copy-dylibs.sh
+++ scripts/copy-dylibs.sh
@@ -5,7 +5,7 @@ mkdir dylibs
 # copy GDAL dependencies
 echo "Copying GDAL dependencies"
 cp /opt/buildhome/miniconda3/envs/gdal_env/lib/libgdal.so* dylibs/
-"$HOME"/miniconda3/bin/conda run -n gdal_env ldd /opt/buildhome/miniconda3/envs/gdal_env/lib/libgdal.so | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
+"$HOME"/miniconda3/bin/conda run -n gdal_env ldd /opt/buildhome/miniconda3/envs/gdal_env/lib/libgdal.so | grep '=>' | grep 'miniconda3' | grep -v " not " | awk '{print $3}' | while read -r lib; do
   echo "Copying $lib"
   cp "$lib" dylibs/
 done
@@ -13,7 +13,7 @@ done
 # copy .node dependencies
 echo "Copying .node dynamic dependencies"
 "$HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node
-"$HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
+"$HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | grep 'miniconda3' | grep -v " not " | awk '{print $3}' | while read -r lib; do
   echo "Copying $lib"
   cp "$lib" dylibs/
 done
