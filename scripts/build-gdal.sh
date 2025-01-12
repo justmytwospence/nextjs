4fb750f3600bcbff34f22dc7fe3c7430998e73a5
Spencer Boucher <github@spencerboucher.com>
2025-01-12 10:14:27 -0700

    Remove gdal from conda environment creation in build-gdal.sh

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -8,8 +8,8 @@ curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Li
 bash miniconda.sh -b > /dev/null
 "$HOME"/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge \
   clang \
-  gdal \
   libcxx \
+  # libgdal \
   libcxxabi \
   libdeflate \
   libjxl \
