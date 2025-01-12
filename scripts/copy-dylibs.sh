f298c1f2728adf8d9d121d0db7c9e8503ffbaec2
Spencer Boucher <github@spencerboucher.com>
2025-01-11 21:23:05 -0700

    Fix library path extraction in copy-dylibs script

--- scripts/copy-dylibs.sh
+++ scripts/copy-dylibs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ldd /opt/build/repo/pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | awk '{print $4}' | while read -r lib; do
+ldd /opt/build/repo/pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | awk '{print $3}' | while read -r lib; do
     cp "$lib" /opt/build/repo/dylibs/
 done
 
