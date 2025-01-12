407f85adb99d2f653b1bca71e48da5432d871006
Spencer Boucher <github@spencerboucher.com>
2025-01-11 22:21:42 -0700

    Update copy-dylibs.sh to specify GDAL and .node dynamic dependencies during copying

--- scripts/copy-dylibs.sh
+++ scripts/copy-dylibs.sh
@@ -3,7 +3,14 @@
 set -e  # Exit on error
 
 mkdir dylibs
-echo "Copying dynamic dependencies"
+
+echo "Copying gdal dynamic dependencies"
+"$HOME"/miniconda3/bin/conda run -n gdal_env ldd libgdal.a | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
+  echo "Copying $lib"
+  cp "$lib" /opt/build/repo/dylibs/
+done
+
+echo "Copying .node dynamic dependencies"
 "$HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
   echo "Copying $lib"
   cp "$lib" /opt/build/repo/dylibs/
