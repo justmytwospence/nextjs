89f97c29412b54aa7be30583246eb08b990eb2ea
Spencer Boucher <github@spencerboucher.com>
2025-01-11 22:15:48 -0700

    Enable GEOS and WEBP support in build-gdal.sh; update copy-dylibs.sh to use conda environment for ldd

--- scripts/copy-dylibs.sh
+++ scripts/copy-dylibs.sh
@@ -4,7 +4,7 @@ set -e  # Exit on error
 
 mkdir dylibs
 echo "Copying dynamic dependencies"
-ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
+"$HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
   echo "Copying $lib"
   cp "$lib" /opt/build/repo/dylibs/
 done
