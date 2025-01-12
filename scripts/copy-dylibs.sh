599981f508fb9ed7456b02a0cc0f483fbda65989
Spencer Boucher <github@spencerboucher.com>
2025-01-11 21:45:31 -0700

    Filter out invalid library paths in copy-dylibs script

--- scripts/copy-dylibs.sh
+++ scripts/copy-dylibs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir dylibs
-ldd pathfinder/pathfinder.linux-x64-gnu.node | tee >(cat) | grep '=>' | awk '{print $3}' | while read -r lib; do
+ldd pathfinder/pathfinder.linux-x64-gnu.node | tee >(cat) | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
   cp "$lib" /opt/build/repo/dylibs/
 done
