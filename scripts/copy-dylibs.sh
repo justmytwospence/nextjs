289dcd2d73fb096bc08e3f75aa6afce1fe182536
Spencer Boucher <github@spencerboucher.com>
2025-01-11 21:37:15 -0700

    Improve library path extraction in copy-dylibs script to include output logging

--- scripts/copy-dylibs.sh
+++ scripts/copy-dylibs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mkdir dylibs
-ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | awk '{print $3}' | while read -r lib; do
+ldd pathfinder/pathfinder.linux-x64-gnu.node | tee >(cat) | grep '=>' | awk '{print $3}' | while read -r lib; do
   cp "$lib" /opt/build/repo/dylibs/
 done
-
