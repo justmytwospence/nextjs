e619dd5da41bef9055bdb13716e17777816b18bf
Spencer Boucher <github@spencerboucher.com>
2025-01-11 23:36:34 -0700

    Fix dynamic library copying script to use 'ldd' instead of 'libtree'

--- scripts/copy-dylibs.sh
+++ scripts/copy-dylibs.sh
@@ -5,7 +5,7 @@ set -e  # Exit on error
 mkdir dylibs
 
 echo "Copying .node dynamic dependencies"
-"$HOME"/miniconda3/bin/conda run -n gdal_env libtree pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
+"$HOME"/miniconda3/bin/conda run -n gdal_env ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | grep -v " not " | awk '{print $3}' | while read -r lib; do
   echo "Copying $lib"
   cp "$lib" /opt/build/repo/dylibs/
 done
