c772aff79e737f791b9d7cdfb026dedeb00dc707
Spencer Boucher <github@spencerboucher.com>
2025-01-12 11:59:32 -0700

    Enable GDAL dynamic linking by uncommenting rpath argument in build script

--- pathfinder/build.rs
+++ pathfinder/build.rs
@@ -4,7 +4,7 @@ fn main() {
   // println!("cargo:rustc-link-search=native=/opt/build/repo");
   // println!("cargo:rustc-link-lib=static=gdal");
   // println!("cargo:rustc-link-arg=-Wl,-rpath,/opt/buildhome/miniconda3/envs/gdal_env/lib");
-  // println!("cargo:rustc-link-arg=-Wl,-rpath,/var/task/dylibs");
+  println!("cargo:rustc-link-arg=-Wl,-rpath,/var/task/dylibs");
   println!("cargo:rustc-link-lib=dylib=gdal");
   println!("cargo:rustc-link-search=native=/opt/buildhome/miniconda3/envs/gdal_env/lib");
   napi_build::setup();
