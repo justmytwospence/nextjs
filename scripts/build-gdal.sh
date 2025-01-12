e83ec4c3a87447bee09f1e8b8aff5ff2bc775039
Spencer Boucher <github@spencerboucher.com>
2025-01-12 13:10:03 -0700

    Refactor build script to use CONDA_DIR variable for conda commands and update RPATH settings for GDAL

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -7,13 +7,13 @@ NUM_CPUS=$(nproc)
 echo "Number of CPUs: $NUM_CPUS"
 
 # dependencies
-MINICONDA_DIR="$HOME"/miniconda3
+CONDA_DIR="$HOME"/miniconda3
 curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash miniconda.sh -b > /dev/null
 
-"$MINICONDA_DIR"/bin/conda install --quiet -c conda-forge libtree patchelf -y
+"$CONDA_DIR"/bin/conda install --quiet -c conda-forge libtree patchelf -y
 
-"$MINICONDA_DIR"/bin/conda create --quiet -n gdal_env -c conda-forge \
+"$CONDA_DIR"/bin/conda create --quiet -n gdal_env -c conda-forge \
   clang \
   gdal \
   geos \
@@ -106,12 +106,16 @@ curl -LO https://github.com/OSGeo/gdal/releases/download/v3.10.0/gdal-3.10.0.tar
 tar -xf gdal-3.10.0.tar.gz
 cd gdal-3.10.0
 mkdir build && cd build
-"$MINICONDA_DIR"/bin/conda run -n gdal_env cmake .. \
+"$CONDA_DIR"/bin/conda run -n gdal_env cmake .. \
   -DBUILD_APPS=OFF \
   -DBUILD_PYTHON_BINDINGS=OFF \
   -DBUILD_SHARED_LIBS=ON \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_INSTALL_PREFIX="$MINICONDA_DIR"/envs/gdal_env \
+  -DCMAKE_CXX_FLAGS="-I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
+  -DCMAKE_C_FLAGS="-I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
+  -DCMAKE_EXE_LINKER_FLAGS="-L$CONDA_DIR/envs/gdal_env/lib" \
+  -DCMAKE_INSTALL_PREFIX="$CONDA_DIR"/envs/gdal_env \
+  -DCMAKE_SHARED_LINKER_FLAGS="-L$CONDA_DIR/envs/gdal_env/lib" \
   -DGDAL_BUILD_OPTIONAL_DRIVERS=OFF \
   -DGDAL_ENABLE_DRIVER_GTIFF=ON \
   -DGDAL_ENABLE_DRIVER_MEM=ON \
@@ -133,16 +137,16 @@ mkdir build && cd build
   # -DPROJ_LIBRARY_RELEASE=../../libproj.a 
 
 echo "Building GDAL"
-"$MINICONDA_DIR"/bin/conda run -n gdal_env cmake --build . --target install -- -j"${NUM_CPUS}"
-"$MINICONDA_DIR"/bin/patchelf --set-rpath /var/task/dylibs "$MINICONDA_DIR"/envs/gdal_env/lib/libgdal.so.36.3.10.0
-"$MINICONDA_DIR"/bin/readelf -d "$MINICONDA_DIR"/envs/gdal_env/lib/libgdal.so.36.3.10.0
+"$CONDA_DIR"/bin/conda run -n gdal_env cmake --build . --target install -- -j"${NUM_CPUS}"
+"$CONDA_DIR"/bin/patchelf --set-rpath /var/task/dylibs "$CONDA_DIR"/envs/gdal_env/lib/libgdal.so.36.3.10.0
 cd ../..
 rm -rf gdal-3.10.0
 
-ls -lah "$MINICONDA_DIR"/envs/gdal_env
-ls -lah "$MINICONDA_DIR"/envs/gdal_env/lib
-ls -lah "$MINICONDA_DIR"/envs/gdal_env/include
+ls -lah "$CONDA_DIR"/envs/gdal_env
+ls -lah "$CONDA_DIR"/envs/gdal_env/lib
+ls -lah "$CONDA_DIR"/envs/gdal_env/include
 
-"$MINICONDA_DIR"/bin/conda run -n gdal_env ldd "$MINICONDA_DIR"/envs/gdal_env/lib/libgdal.so 
+"$CONDA_DIR"/bin/conda run -n gdal_env ldd "$CONDA_DIR"/envs/gdal_env/lib/libgdal.so 
+"$CONDA_DIR"/bin/conda run -n gdal_env ldd "$CONDA_DIR"/envs/gdal_env/lib/libm.so.6
 
 rustup default stable
