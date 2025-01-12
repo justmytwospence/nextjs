572dd3cc9f0f22e69ac2898c6c3e5c564b795f48
Spencer Boucher <github@spencerboucher.com>
2025-01-11 17:17:52 -0700

    Remove explicit makefile specification in GDAL build script for libwebp

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -35,7 +35,7 @@ mkdir build && cd build
 echo "Building libwebp"
 cmake .. \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-make -j4 -f makefile.unix
+make -j4 
 mv src/libwebp.a ../..
 cd ../..
 rm -rf libwebp-1.5.0
