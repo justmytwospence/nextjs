dfca4add82f74c147a40a30bdbe2b89c667e2e5a
Spencer Boucher <github@spencerboucher.com>
2025-01-12 12:21:51 -0700

    Install libtree and patchelf in the build script for GDAL

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -9,6 +9,9 @@ echo "Number of CPUs: $NUM_CPUS"
 # dependencies
 curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash miniconda.sh -b > /dev/null
+
+"$HOME"/miniconda3/bin/conda install -c conda-forge libtree patchelf -y
+
 "$HOME"/miniconda3/bin/conda create --quiet -n gdal_env -c conda-forge \
   clang \
   gdal \
@@ -19,8 +22,6 @@ bash miniconda.sh -b > /dev/null
   libdeflate \
   libjxl \
   libtiff \
-  libtree \
-  patchelf \
   pkg-config \
   proj -y
 
