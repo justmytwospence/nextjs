89792189191a1fafa443a7a30f02ba90f127d0e4
Spencer Boucher <github@spencerboucher.com>
2025-01-11 21:28:49 -0700

    Create dylibs directory before copying dynamic libraries in copy-dylibs script

--- scripts/copy-dylibs.sh
+++ scripts/copy-dylibs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ldd /opt/build/repo/pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | awk '{print $3}' | while read -r lib; do
-    cp "$lib" /opt/build/repo/dylibs/
+mkdir dylibs
+ldd pathfinder/pathfinder.linux-x64-gnu.node | grep '=>' | awk '{print $3}' | while read -r lib; do
+  cp "$lib" /opt/build/repo/dylibs/
 done
 
