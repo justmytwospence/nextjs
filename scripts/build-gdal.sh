cb4b85a32f3effd17b40a822c53385da5579766a
Spencer Boucher <github@spencerboucher.com>
2025-01-11 19:59:57 -0700

    Update GDAL build script to enable static position-independent executable

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -20,6 +20,7 @@ mkdir build && cd build
 echo "Building libc"
 $HOME/miniconda3/bin/conda run -n gdal_env ../configure \
   --prefix=/opt/build/repo/glibc-2.40/build \
+  --enable-static-pie \
   CFLAGS="-O2 -fPIC -Wno-error=attributes" \
   CXXFLAGS="-O2 -fPIC"
 make -j4
