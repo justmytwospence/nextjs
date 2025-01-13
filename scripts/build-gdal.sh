4a4c8dc4fb698f2ae40e03279d60e64d0ee6e3e8
Spencer Boucher <github@spencerboucher.com>
2025-01-12 18:18:41 -0700

    Revert to 6b3a1c3

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -15,42 +15,55 @@ bash miniconda.sh -b > /dev/null
 "$CONDA_DIR"/bin/conda install --quiet -c conda-forge libtree patchelf -y
 
 "$CONDA_DIR"/bin/conda create --quiet -n gdal_env -c conda-forge \
+  clang \
   cmake \
   geos \
-  gxx_linux-64 libgcc-ng libstdcxx-ng \
   libcurl \
   libcxxabi \
   libdeflate \
   libjxl \
-  libsqlite \
   libtiff \
   pkg-config \
   proj -y
 
-  # clang libcxx \
+  # gxx_linux-64 \
+  # libcxx \
 
-ls -lah "$CONDA_DIR"/envs/gdal_env/bin
 "$CONDA_DIR"/bin/conda list glibc
 "$CONDA_DIR"/bin/conda list gxx_linux-64
 "$CONDA_DIR"/bin/conda run -n gdal_env ldd --version
+
+# # libc 
+# curl -LO https://ftp.gnu.org/gnu/libc/glibc-2.40.tar.xz
+# tar xf glibc-2.40.tar.xz
+# cd glibc-2.40
+# mkdir build && cd build
+# echo "Building libc"
+# $HOME/miniconda3/bin/conda run -n gdal_env ../configure \
+#   --prefix=/opt/build/repo/glibc-2.40/build \
+#   --enable-static-pie \
+#   CFLAGS="-O2 -fPIC -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0 -Wno-error=attributes" \
+#   CXXFLAGS="-O2 -fPIC"
+# make -j4
+# mv libc.a ../..
+# cd ../..
+# rm -rf glibc-2.40
 
 # gdal
 curl -LO https://github.com/OSGeo/gdal/releases/download/v3.10.0/gdal-3.10.0.tar.gz
 tar -xf gdal-3.10.0.tar.gz
 cd gdal-3.10.0
 mkdir build && cd build
-echo "Building GDAL"
-echo "$LD_LIBRARY_PATH"
 "$CONDA_DIR"/bin/conda run -n gdal_env cmake .. \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_FLAGS=" -I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
-  -DCMAKE_C_FLAGS=" -I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
-  -DCMAKE_INSTALL_PREFIX="$CONDA_DIR"/envs/gdal_env \
-  -DACCEPT_MISSING_LINUX_FS_HEADER=ON \
-  -DACCEPT_MISSING_SQLITE3_MUTEX_ALLOC=ON \
   -DBUILD_APPS=OFF \
   -DBUILD_PYTHON_BINDINGS=OFF \
   -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_FLAGS="-I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
+  -DCMAKE_C_FLAGS="-I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
+  -DCMAKE_EXE_LINKER_FLAGS="-L$CONDA_DIR/envs/gdal_env/lib" \
+  -DCMAKE_INSTALL_PREFIX="$CONDA_DIR"/envs/gdal_env \
+  -DCMAKE_SHARED_LINKER_FLAGS="-L$CONDA_DIR/envs/gdal_env/lib" \
   -DGDAL_BUILD_OPTIONAL_DRIVERS=OFF \
   -DGDAL_ENABLE_DRIVER_GTIFF=ON \
   -DGDAL_ENABLE_DRIVER_MEM=ON \
@@ -58,20 +71,20 @@ echo "$LD_LIBRARY_PATH"
   -DGDAL_USE_DEFLATE=ON \
   -DGDAL_USE_GEOS=ON \
   -DGDAL_USE_INTERNAL_LIBS=ON \
-  -DGDAL_USE_JPEG=OFF \
   -DGDAL_USE_WEBP=ON \
   -DOGR_BUILD_OPTIONAL_DRIVERS=OFF
 
-  # -DCMAKE_C_COMPILER="$CONDA_DIR"/envs/gdal_env/bin/x86_64-conda-linux-gnu-gcc \
-  # -DCMAKE_CXX_COMPILER="$CONDA_DIR"/envs/gdal_env/bin/x86_64-conda-linux-gnu-g++ \
-  # -DCMAKE_SHARED_LINKER_FLAGS="-L$CONDA_DIR/envs/gdal_env/lib" \
-  # -DCMAKE_EXE_LINKER_FLAGS="-L$CONDA_DIR/envs/gdal_env/lib" \
-  # -DCMAKE_CXX_FLAGS="-nostdlib --sysroot=$CONDA_DIR/envs/gdal_env -I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
-  # -DCMAKE_C_FLAGS="-nostdlib --sysroot=$CONDA_DIR/envs/gdal_env -I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
   # -DCMAKE_CXX_FLAGS="-fPIC" \
   # -DCMAKE_C_FLAGS="-fPIC" \
   # -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+  # -DBUILD_SHARED_LIBS=OFF \
+  # -DDeflate_LIBRARY_RELEASE=../../libdeflate.a \
+  # -DGDAL_USE_WEBP=ON \
+  # -DWEBP_LIBjARARY=../../libwebp.a \
+  # -DGEOS_LIBRARY=../../libgeos_c.a \
+  # -DPROJ_LIBRARY_RELEASE=../../libproj.a 
 
+echo "Building GDAL"
 "$CONDA_DIR"/bin/conda run -n gdal_env cmake --build . --target install -- -j"${NUM_CPUS}"
 "$CONDA_DIR"/bin/patchelf --set-rpath /var/task/dylibs "$CONDA_DIR"/envs/gdal_env/lib/libgdal.so.36.3.10.0
 cd ../..
