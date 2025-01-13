c0925b9cf3935159f8e158e4826f50b5a4fb95e4
Spencer Boucher <github@spencerboucher.com>
2025-01-12 17:58:44 -0700

    Remove sysroot specification from compiler flags in GDAL build script

--- scripts/build-gdal.sh
+++ scripts/build-gdal.sh
@@ -44,9 +44,9 @@ echo "$LD_LIBRARY_PATH"
 "$CONDA_DIR"/bin/conda run -n gdal_env cmake .. \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_CXX_COMPILER="$CONDA_DIR"/envs/gdal_env/bin/x86_64-conda-linux-gnu-g++ \
-  -DCMAKE_CXX_FLAGS="--sysroot=$CONDA_DIR/envs/gdal_env -I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
+  -DCMAKE_CXX_FLAGS=" -I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
   -DCMAKE_C_COMPILER="$CONDA_DIR"/envs/gdal_env/bin/x86_64-conda-linux-gnu-gcc \
-  -DCMAKE_C_FLAGS="--sysroot=$CONDA_DIR/envs/gdal_env -I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
+  -DCMAKE_C_FLAGS=" -I$CONDA_DIR/envs/gdal_env/include -L$CONDA_DIR/envs/gdal_env/lib" \
   -DCMAKE_INSTALL_PREFIX="$CONDA_DIR"/envs/gdal_env \
   -DBUILD_APPS=OFF \
   -DBUILD_PYTHON_BINDINGS=OFF \
