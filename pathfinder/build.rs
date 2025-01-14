cb7bd7f34d8f777e4efeca8b488f55b843e60edc
Spencer Boucher <github@spencerboucher.com>
2025-01-13 18:34:31 -0700

    Remove commented-out build configuration lines in build.rs

--- pathfinder/build.rs
+++ pathfinder/build.rs
@@ -1,8 +1,5 @@
 extern crate napi_build;
 
 fn main() { 
-  // println!("cargo:rustc-link-search=native=/opt/build/repo");
-  // println!("cargo:rustc-link-lib=static=gdal");
-  // println!("cargo:rustc-link-arg=-Wl,-rpath,/opt/buildhome/miniconda3/envs/gdal_env/lib");
   napi_build::setup();
 }
