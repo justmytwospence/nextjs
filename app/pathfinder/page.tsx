21fe8634aba924bbfd6e77fd6bc04d5ad14edd1b
Spencer Boucher <github@spencerboucher.com>
2025-01-15 19:56:19 -0700

    Reset path and aspect points on new path set in PathFinderPage

--- app/pathfinder/page.tsx
+++ app/pathfinder/page.tsx
@@ -73,6 +73,8 @@ export default function PathFinderPage() {
   }
 
   const handleSetPath = useCallback((newPath: LineString | null) => {
+    setPath(null);
+    setAspectPoints(null)
     setPath((currentPath) => {
       if (newPath === null) {
         return null;
