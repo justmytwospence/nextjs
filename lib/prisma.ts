b9432612b0c8dd17690c14d5c3b38ede44c8d37e
Spencer Boucher <github@spencerboucher.com>
2025-01-27 09:03:59 -0700

    Switch back to Supabase

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
-
-// Client for long-running transactions
-export const longPrisma = new PrismaClient({
-  transactionOptions: {
-    timeout: 300000, // 5 minutes
-  },
-});
